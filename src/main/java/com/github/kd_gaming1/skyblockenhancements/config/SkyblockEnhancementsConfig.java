7abcfaff8b8092e9f6b630d1b7c34a688d888aac
Kd_Gaming1 <97355992+KdGaming0@users.noreply.github.com>
2026-02-04 12:56:22 +0100

    Add configuration and localization for Skyblock Enhancements

---
+++ src/main/java/com/github/kd_gaming1/skyblockenhancements/config/SkyblockEnhancementsConfig.java
@@ -0,0 +1,15 @@
+package com.github.kd_gaming1.skyblockenhancements.config;
+
+import eu.midnightdust.lib.config.MidnightConfig;
+
+public class SkyblockEnhancementsConfig extends MidnightConfig {
+    public static final String SKYBLOCK_ENCHANTMENTS = "skyblock_enhancements";
+
+    @Comment(category = SKYBLOCK_ENCHANTMENTS, centered = true)
+    public static Comment text;
+
+    @Entry(category = SKYBLOCK_ENCHANTMENTS)
+    public static boolean showMissingEnchantments = true;
+    @Entry(category = SKYBLOCK_ENCHANTMENTS)
+    public static boolean showWhenPressingShift = true;
+}
