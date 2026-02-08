ec9c355c62a27791acc90552a08bd56386612e72
KdGaming0 <97355992+KdGaming0@users.noreply.github.com>
2026-02-08 15:33:34 +0100

    fix: Add fabric_api dependency to build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -73,6 +73,7 @@ tasks {
         inputs.property("version", project.property("mod.version"))
         inputs.property("minecraft", project.property("mod.mc_dep"))
         inputs.property("fabricloader", project.property("deps.fabric_loader"))
+        inputs.property("fabric_api", project.property("deps.fabric_api"))
         inputs.property("moulconfig", project.property("deps.moulconfig_version"))
         inputs.property("hm_api", project.property("deps.hm_api_version"))
 
@@ -82,6 +83,7 @@ tasks {
             "version" to project.property("mod.version"),
             "minecraft" to project.property("mod.mc_dep"),
             "fabricloader" to project.property("deps.fabric_loader"),
+            "fabric_api" to project.property("deps.fabric_api"),
             "moulconfig" to project.property("deps.moulconfig_version"),
             "hm_api" to project.property("deps.hm_api_version"),
         )
